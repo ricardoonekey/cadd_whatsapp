5e6c119c864cc7c529332ae451bdbb2de1aa69e3
Ricardo Rios <ricardo.rios@onekey.com.mx>
2024-12-04 16:13:11 -0600

    add: Insert new interaction on database with engagement_id and timestamp

--- __main__.py
+++ __main__.py
@@ -1,5 +1,8 @@
+from os import abort
+
 import psycopg2
 from flask import Flask, request, jsonify
+from datetime import datetime
 import configparser
 
 # Initiating a Flask application
@@ -26,10 +29,10 @@ def execute_query(query, params=None, fetchall=False, fetchone=False):
                 if fetchall:
                     return db.fetchall()
                 if fetchone:
-                    return db.fetchone()
+                    return db.fetchone()[0]
                 cnx.commit()
                 if query.strip().upper().startswith("INSERT"):
-                    return db.lastrowid
+                    return db.fetchone()[0]
     except Exception as e:
         raise e
 
@@ -45,6 +48,19 @@ def handle_request():
         response = {'records': records}
         return jsonify(response)
 
+@app.route(rule="/new_interaction", methods=["POST"])
+def insert_interaction():
+    if request.method == "POST":
+        payload = request.get_json()
+        try:
+            insertion_id = execute_query(
+                "INSERT INTO whatsapp_requests (engagement_id, created) VALUES (%s, %s) RETURNING id",
+                (payload['engagement_id'], datetime.now())
+            )
+            return jsonify(insertion_id)
+        except Exception as e:
+            return f"There was an error: {e}", 500
+
 # Running the API
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
